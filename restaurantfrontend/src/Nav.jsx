4c1e59a50c313bc800f26d0e2390a7b5af29d1c9
kenenwobodo <nwobodokenechukwu2@gmail.com>
2020-07-25 11:50:34 +0100

    Added styles

--- restaurantfrontend/src/Nav.jsx
+++ restaurantfrontend/src/Nav.jsx
@@ -5,22 +5,22 @@ class Nav extends Component {
     
     render() { 
         return ( 
-            <nav className="navbar navbar-expand-lg navbar-dark  bg-danger">
-            <Link to="/" className="text-white nav-link" ><img className="logo" src="https://res.cloudinary.com/dtu2furcy/image/upload/v1595640391/migrant%20solutions%20project/Chef-removebg-preview_xw5b3e.png" alt="chef logo"/></Link>
+            <nav className="navbar navbar-expand-lg navbar-light">
+            <Link to="/" className="nav-link" ><img className="logo" src="https://res.cloudinary.com/dtu2furcy/image/upload/v1595640391/migrant%20solutions%20project/Chef-removebg-preview_xw5b3e.png" alt="chef logo"/></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav mx-auto">
+                <ul className="navbar-nav mx-auto nav nav-tabs ">
                 
                 <li className="nav-item">
-                    <Link to="/" className="nav-link active" href="#">Yummy</Link>
+                    <Link to="/" className="nav-link text-danger active">Menu</Link>
                 </li>
                 <li className="nav-item">
-                    <Link to="/create" className="nav-link text-white" href="#">Order</Link>
+                    <Link to="/create" className="nav-link text-danger">Order</Link>
                 </li>
                 <li className="nav-item">
-                <Link to="/signup" className="nav-link text-white" href="#">Signup</Link>
+                <Link to="/signup" className="nav-link text-danger">Signup</Link>
                 </li>
                 </ul>
                 
--- restaurantfrontend/src/Yummy.jsx
+++ restaurantfrontend/src/Yummy.jsx
@@ -37,20 +37,74 @@ class Dishes extends Component {
     render() { 
         return ( 
             <div>
-              <table className="table">
-                  <thead className="thead-dark">
-                    <tr>
-                        <th>Customer's Username</th>
-                        <th>Dish Ordered</th>
-                        <th>Time Ordered</th>
-                        <th>Date Ordered</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                      {this.dishKitchen()}
-                  </tbody>
-              </table>
-              
+                <div className="container menu-container">
+                    <h2>Explore Our Menu</h2>
+                 <div className="row">
+                  <div className="col-md-4">
+                  <div className="card">
+                    <img src="https://res.cloudinary.com/dtu2furcy/image/upload/v1595671163/migrant%20solutions%20project/Rectangle_66_jey3p7.png" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h2>Irish COffe</h2>
+                        <p className="card-text">N1200</p>
+                    </div>
+                    </div>
+                  </div>
+                  
+                  <div className="col-md-4">
+                  <div className="card">
+                    <img src="https://res.cloudinary.com/dtu2furcy/image/upload/v1595671163/migrant%20solutions%20project/Rectangle_52_1_k1ymhf.png" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h2>Yummy Yogo</h2>
+                        <p className="card-text">N2000</p>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div className="col-md-4">
+                  <div className="card">
+                    <img src="https://res.cloudinary.com/dtu2furcy/image/upload/v1595671163/migrant%20solutions%20project/Rectangle_52_i82ehg.png" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h2>Argention Cake</h2>
+                        <p className="card-text">N5000</p>
+                    </div>
+                    </div>
+                  </div>
+                 </div>
+                </div>
+
+               
+                 <div className="row second-row">
+                  <div className="col-md-4">
+                  <div className="card">
+                    <img src="https://res.cloudinary.com/dtu2furcy/image/upload/v1595671163/migrant%20solutions%20project/Rectangle_66_1_rqrbfh.png" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h2>San Coffee</h2>
+                        <p className="card-text">N1500</p>
+                    </div>
+                    </div>
+                  </div>
+                  
+                  <div className="col-md-4">
+                  <div className="card">
+                    <img src="https://res.cloudinary.com/dtu2furcy/image/upload/v1595671163/migrant%20solutions%20project/Rectangle_56_ijynqv.png" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h2>Caffe Americano</h2>
+                        <p className="card-text">N1000</p>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div className="col-md-4">
+                  <div className="card">
+                    <img src="https://res.cloudinary.com/dtu2furcy/image/upload/v1595671163/migrant%20solutions%20project/Rectangle_54_fwp4q7.png" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h2>Filter Coffe</h2>
+                        <p className="card-text">N1400</p>
+                    </div>
+                    </div>
+                  </div>
+                
+                </div>
             </div>
          );
     }
